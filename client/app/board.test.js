3be22048559cd65170ff720260085af21c67b336
hleetwin <leetwin.h@gmail.com>
2016-03-21 15:49:14 -0700

    set up sample test

--- client/app/board.test.js
+++ client/app/board.test.js
@@ -4,6 +4,7 @@ module.exports = function(ngModule) {
   describe('board', function() {
     beforeEach(window.module(ngModule.name));
 
+      //testing whether karma set up properly
       it('should test properly', function() {
         expect(true).to.be.true;
       });
